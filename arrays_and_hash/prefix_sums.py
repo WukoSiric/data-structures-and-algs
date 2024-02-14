392331fa7b9459fa4e8da9a1017e801588590bdc
Luke Stainer <lstainer2002@gmail.com>
2024-02-14 20:55:36 +1030

    fixed indenting

--- arrays_and_hash/prefix_sums.py
+++ arrays_and_hash/prefix_sums.py
@@ -1,13 +1,11 @@
 def get_prefix_sum_array(array: list[str]): 
     prefix_array = list()
-
     for i in range(len(array)): 
         if i == 0: 
             prefix_array.append(array[0])
         else: 
             prefix_array.append(array[i] + prefix_array[i-1])
     return prefix_array
-
 
 def main():
     sample_array = [1, 2, 80, 100]
