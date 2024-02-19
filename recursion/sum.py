69137303f48093ef6d8e0651f31faf599bc6f485
Luke Stainer <lstainer2002@gmail.com>
2024-02-19 16:38:04 +1030

    Did another practcie recursion

--- recursion/sum.py
+++ recursion/sum.py
@@ -1,22 +1,24 @@
-def findSum(nums: list[int], sum):
-
+def findSum(nums: list[int]):
     # base case
     if len(nums) <= 1: 
-        return sum + nums[0]
+        return nums[0]
+
+    return nums[0] + findSum(nums[1:])
+
+
+def findIntSum(number: int): 
+    number = str(number)
+
+    if len(number) <= 1: 
+        return int(number)
     
+    return int(number[0]) + findIntSum(int(number[1:]))
 
-    sum += nums[0]
-    nums.pop(0)
-
-    return findSum(nums, sum)
-
-
-# findsum(array, 0)
-# sum = 
 
 def main(): 
     array = [1, 4, 5, 6]
-    print(findSum(array, 0))
+    print(findSum(array))
+    print(findIntSum(45))
 
 if __name__ == "__main__": 
     main()
